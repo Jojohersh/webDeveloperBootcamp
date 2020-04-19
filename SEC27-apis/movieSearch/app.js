838263ab44a88761f52bee0d821e8a4c0a340ff9
Jordan <jojohersh@gmail.com>
2020-04-19 12:26:30 -0400

    played with apis

---
+++ SEC27-apis/movieSearch/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const request = require("request");
+app.set("view engine", "ejs");
+
+const apiKey = "&apikey=thewdb";
+
+app.get("/", (req,res) => {
+  var query = "?s=";
+  res.render("search");
+});
+
+app.get("/results", (req,res) => {
+  var query = req.query.search;
+  var url = `http://omdbapi.com/?s=${query}&apikey=thewdb`
+  request(url, (error, response, body) => {
+      if (!error && response.statusCode == 200) {
+        var data = JSON.parse(body);
+        res.render("results", {data:data});
+      }
+  });
+});
+
+app.listen(3000, () => {
+  console.log("server running on port 3000...");
+})
