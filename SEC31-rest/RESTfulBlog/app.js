2d9a919eb8a2430425c316dab0287974a0a5a32e
Jordan <jojohersh@gmail.com>
2020-04-27 14:31:09 -0400

    added full REST funcitonality

--- SEC31-rest/RESTfulBlog/app.js
+++ SEC31-rest/RESTfulBlog/app.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override')
 //  APP CONFIG =========================================
 const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
 
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useUnifiedTopology', true);
+mongoose.set('useFindAndModify', false);
 mongoose.connect("mongodb://localhost/restfulBlogApp");
 
 //MONGOOSE MODEL ========================================
@@ -19,8 +22,8 @@ var blogSchema = new mongoose.Schema({
   body: String,
   created: {type:Date, default: Date.now}
 })
-var blog = mongoose.model("Blog", blogSchema);
-// blog.create({
+var Blog = mongoose.model("Blog", blogSchema);
+// Blog.create({
 //   title: "test blog",
 //   image: "https://cdn.pixabay.com/photo/2016/03/09/09/43/person-1245959_960_720.jpg",
 //   body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
@@ -30,7 +33,7 @@ app.get("/", (req,res)=> {
   res.redirect("/blogs");
 });
 app.get("/blogs", (req,res)=>{
-  var blogs = blog.find({}, (err, blogs)=>{
+  var blogs = Blog.find({}, (err, blogs)=>{
     if (err) {
       console.log(err);
     } else {
@@ -46,7 +49,7 @@ app.get("/blogs/new", (req,res)=>{
 app.post("/blogs", (req,res)=>{
   // create blog
   var data = req.body.blog;
-  blog.create(data, (err,newBlog) =>{
+  Blog.create(data, (err,newBlog) =>{
     if (err) {
       res.render("new");
     } else {
@@ -55,6 +58,49 @@ app.post("/blogs", (req,res)=>{
     }
   });
 });
+//SHOW ROUTE
+app.get("/blogs/:id", (req,res)=>{
+  Blog.findById(req.params.id, (err,foundBlog)=>{
+    if(err) {
+      res.redirect("/blogs");
+    } else {
+      res.render("show", {blog:foundBlog});
+    }
+  });
+});
+//EDIT ROUTE
+app.get("/blogs/:id/edit", (req,res)=>{
+  Blog.findById(req.params.id, (err,foundBlog)=>{
+    if(err) {
+      console.log(err);
+    } else {
+      res.render("edit",{blog:foundBlog});
+    }
+  });
+});
+//UPDATE ROUTE
+app.put("/blogs/:id", (req,res)=>{
+  Blog.findByIdAndUpdate(req.params.id,req.body.blog, (err,updatedBlog)=>{
+    if(err) {
+      res.redirect("/blogs/");
+    } else {
+      res.redirect("/blogs/" + req.params.id);
+    }
+  });
+});
+//DESTROY ROUTE
+app.delete("/blogs/:id", (req,res)=>{
+  //destroy blog
+  Blog.findByIdAndRemove(req.params.id, (err)=>{
+    if(err) {
+      res.redirect("/blogs");
+    } else {
+      res.redirect("/blogs");
+    }
+  });
+  //redirect somewhere
+});
+
 app.listen("3000", ()=> {
   console.log("Server running on port 3000...");
 });
